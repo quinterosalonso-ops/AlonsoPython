ae2ef50d24d0d3e5b49725160638e0cf7177fb46
Alonso Quinteros <quinteros.alonso@gmail.com>
2026-04-22 17:05:06 -0400

    funciones

--- Prueba.py
+++ Prueba.py
@@ -60,35 +60,35 @@
     
 #ejemplo y explicacion de match
 
-# Precios
-prod1 = 70000
-prod2 = 500000
-prod3 = 580000
-iva= 1.19
-op=0
-Total=0
-while op !=4:
-    print(f"1. Radio estereo Sony ${prod1}")
-    print(f"""2. LGTV 55" Super gamer ${prod2} """)
-    print(f"3. PS5 ${prod3}")
-    print(f"4. Salir")
-    print("Seleccione una opcion")
-    op=int(input())
-    match op:
-        case 1:
-            print(f"El valor del producto con iva incluido es de ${prod1*iva}")
-            Total+=prod1
-        case 2:
-            print(f"El valor del producto con iva incluido es de ${prod2*iva}")
-            Total+=prod2
-        case 3:
-            print(f"El valor del producto con iva incluido es de ${prod3*iva}")
-            Total+=prod3
-        case 4:
-            print("Saliendo del sistema")
-        case _:
-            print("Opcion invalida")
-    print(f"su total es de {Total}")
+# # Precios
+# prod1 = 70000
+# prod2 = 500000
+# prod3 = 580000
+# iva= 1.19
+# op=0
+# Total=0
+# while op !=4:
+#     print(f"1. Radio estereo Sony ${prod1}")
+#     print(f"""2. LGTV 55" Super gamer ${prod2} """)
+#     print(f"3. PS5 ${prod3}")
+#     print(f"4. Salir")
+#     print("Seleccione una opcion")
+#     op=int(input())
+#     match op:
+#         case 1:
+#             print(f"El valor del producto con iva incluido es de ${prod1*iva}")
+#             Total+=prod1
+#         case 2:
+#             print(f"El valor del producto con iva incluido es de ${prod2*iva}")
+#             Total+=prod2
+#         case 3:
+#             print(f"El valor del producto con iva incluido es de ${prod3*iva}")
+#             Total+=prod3
+#         case 4:
+#             print("Saliendo del sistema")
+#         case _:
+#             print("Opcion invalida")
+#     print(f"su total es de {Total}")
 
 # cont=1
 # num=9
@@ -96,3 +96,47 @@ while op !=4:
 #     print(f"{cont} x {num} = {cont*num}")
 #     cont+=1
 
+var1=0
+var2=0
+op=int(input('''Ingrse una operacion
+            1.- Suma
+            2.- Resta
+            3.- Multiplicacion
+            4.- Sivision
+            5.- Salir
+            '''))
+
+def PedirNum():
+    global var1
+    global var2
+    var1=int(input("ingresar primer numero de la operacion"))
+    var2=int(input("ingresar segundo numero de la operacion"))
+
+while op!=5:
+    op=int(input('''Ingrse una operacion
+            1.- Suma
+            2.- Resta
+            3.- Multiplicacion
+            4.- Sivision
+            5.- Salir
+            '''))
+    match op:
+        case 1:
+            PedirNum()
+            print("el resultado de la operacion es", var1+var2)
+        case 2:
+            PedirNum()
+            print("el resultado de la operacion es", var1-var2)
+        case 3:
+            PedirNum()
+            print("el resultado de la operacion es", var1*var2)
+        case 4:
+            PedirNum()
+            print("el resultado de la operacion es", var1/var2)
+        case 5:
+            print("Saliendo del sistema")
+        case _:
+            print("opcion invalida")
+    
+
+    
