0821aae0b2b3d83a7b1211933e5c7dda2836231f
Alonso Quinteros <quinteros.alonso@gmail.com>
2026-04-27 11:08:36 -0400

    Mas validaciones

--- randomly.py
+++ randomly.py
@@ -38,43 +38,70 @@ import time
 #     print(f"jugador {i+1} lanza la pelota {tirojugador()} metros en su tiro mas alto")
 #     time.sleep(2)
 
-# dos peleadores se piden al inicio de la pelea
-# cada peleador inicia con 100 de hp
-# se debe hacer una pelea por turnos
-# y cada golpe varia entre 7 y 18
-# se termina el match cuando uno de los 2 tiene su hp menor o igual a 0
-# se debe mostrar el ganador al final
+# # dos peleadores se piden al inicio de la pelea
+# # cada peleador inicia con 100 de hp
+# # se debe hacer una pelea por turnos
+# # y cada golpe varia entre 7 y 18
+# # se termina el match cuando uno de los 2 tiene su hp menor o igual a 0
+# # se debe mostrar el ganador al final
 
-player1name=input("Selecciona tu jugador player 1: ")
-player2name=input("Selecciona tu jugador player 2: ")
+# player1name=input("Selecciona tu jugador player 1: ")
+# player2name=input("Selecciona tu jugador player 2: ")
 
-print(f"Inicia el combate entre {player1name} y {player2name}.")
-player1hp=100
-player2hp=100
-turno=rd.randint(1,2)
+# print(f"Inicia el combate entre {player1name} y {player2name}.")
+# player1hp=100
+# player2hp=100
+# turno=rd.randint(1,2)
 
-while player1hp > 0 and player2hp > 0:
-    if turno % 2 == 0:
-        golpe=rd.randint(7,18)
-        print(f"{player1name} golpea haciendo {golpe} puntos de hp")
-        player2hp-=golpe
-    else:
-        golpe=rd.randint(7,18)
-        print(f"{player2name} golpea haciendo {golpe} puntos de hp")
-        player1hp-=golpe
-    turno+=1
-    player1hptext=("█")*(player1hp)
-    player2hptext=("█")*(player2hp)
-    print(f'''{player1name} {player1hptext}
-{player2name} {player2hptext}''')
-    time.sleep(1)
+# while player1hp > 0 and player2hp > 0:
+#     if turno % 2 == 0:
+#         golpe=rd.randint(7,18)
+#         print(f"{player1name} golpea haciendo {golpe} puntos de hp")
+#         player2hp-=golpe
+#     else:
+#         golpe=rd.randint(7,18)
+#         print(f"{player2name} golpea haciendo {golpe} puntos de hp")
+#         player1hp-=golpe
+#     turno+=1
+#     player1hptext=("█")*(player1hp)
+#     player2hptext=("█")*(player2hp)
+#     print(f'''{player1name} {player1hptext}
+# {player2name} {player2hptext}''')
+#     time.sleep(1)
 
-if player1hp>player2hp:
-    print(f"{player1name} wins.")
-else:
-    print(f"{player2name} wins.")
+# if player1hp>player2hp:
+#     print(f"{player1name} wins.")
+# else:
+#     print(f"{player2name} wins.")
 
+# Crea un numero random entre 1 y 100
+# Pide al usuario que adivine el numero
+# si el usuario pone un numero mayor al generado
+# debe decir "te pasaste", en caso contrario
+# "El numero a adivinar es mayor"
+# solo hay 5 oportunidades de adivinar
 
+numrandom= rd.randint(1,100)
+oportunidad=0
+correcto=False
+while oportunidad<5:
+    numsel=int(input("Ingresa un numero entre 0-100: "))
+    while numsel >100 or numsel<0:
+        numsel=int(input("Numero fuera de rango, intente nuevamente: "))
+    if (numsel) > numrandom:
+        print("Te pasaste")
+    elif numsel < numrandom:
+        print("El numero a adivinar es mayor")
+    elif numsel==numrandom:
+        print("Has adivinado el numero")
+        correcto=True
+    oportunidad+=1
+
+if correcto==False:
+    print("Se acabaron las oportunidades")
+    print(f"el numero era secreto era {numrandom}.")
+
+        
 
 
 
